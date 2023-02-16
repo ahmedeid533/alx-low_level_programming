3f6af6bbb78d3f5b577dfd947e375d74eae3d361
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-02-16 06:48:16 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - first line of code
+ *
+ * return: should return 0 always
+ *
+ * sizeof: return type is lu
+ */
+int main(void)
+{
+	char character;
+	int integr;
+	long int long_integr;
+	long long int long_long_int;
+	float floating_point;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(character));
+	printf("Size of an int: %lu byte(s)\n", sizeof(integr));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long_integr));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long_long_int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(floating_point));
+	return (0);
+}
