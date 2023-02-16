f5bd2de686bb2ccdf8ecc8a2ebc7fd0077e92cf9
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-02-16 06:09:05 -0800

    TEST4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int puts() {
-	printf("Programming is like building a multilingual puzzle\n");
+int main() {
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return 0;
 }
