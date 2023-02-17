6682e182465cbce4979909c755fa9a710dc1f045
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-02-17 10:15:11 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - this app print
+ * characters from a to z
+ * by ascii code term
+ *
+ * Return: 0 mean success
+ *
+ */
+int main(void)
+{
+	char character;
+
+	for (character = 'a'; character <= 'z'; character++)
+	{
+		if (character != 'q' || character != 'e')
+		putchar(character);
+	}
+	putchar('\n');
+	return (0);
+}
