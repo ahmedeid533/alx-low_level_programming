f8377ab73e1e0c126e1ecb01d0acf702943163a4
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-02-20 20:35:01 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio>
+
+/**
+ * main - this function should print 
+ * putchar followed by new line
+ *
+ * Return : should return 0 after success
+ */
+
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
