8b5c48af187770923667de8ceca5994162e72212
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-02-21 06:10:27 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,7 +7,7 @@
  * Return: 1 and prints + if n is greater than zero
  * -Returns 0 and prints 0 if n is zero
  * --Returns -1 and prints - if n is less than zero
- * 
+ *
  * @n: signed number
  */
 
