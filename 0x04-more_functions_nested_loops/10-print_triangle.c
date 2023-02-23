01105e896f11e28f96d693d17868a4b0c4f353a4
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-02-23 06:05:52 -0800

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * followed by a new line.
+ *
+ * Return: void no print
+ *
+ * size: is the size of the triangle
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = i; j < size; j++)
+		{
+			_putchar(' ');
+		}
+		for (k = 1; k <= i; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
