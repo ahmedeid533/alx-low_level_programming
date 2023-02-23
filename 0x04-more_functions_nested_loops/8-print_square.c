4c7756b97c0c04eab958445a008d9ae23c1bc710
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-02-23 05:03:03 -0800

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,4 +8,20 @@
  *
  * @size: is the size of the square
  */
+void print_square(int size)
+{
+	int i;
 
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (i == 0)
+	{
+		_putchar('\n');
+	}
+}
