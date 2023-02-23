45fe15823d2b8244ac2409ade109943cbbcfd5e9
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-02-22 22:44:59 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ *
+ * Return: 1 if c is uppercase
+ * Returns 0 otherwise
+ *
+ * @c: check char
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
