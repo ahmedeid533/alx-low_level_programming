349fa1700f9ef3bc2e1ac8507db0550953c46061
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-02-23 05:46:00 -0800

    return 0

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,20 +17,25 @@ int main(void)
 	{
 		if (!(i % 3) && !(i % 5))
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (!(i % 3))
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (!(i % 5))
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
 		}
 	}
 	printf("\n");
+	return (0);
 }
