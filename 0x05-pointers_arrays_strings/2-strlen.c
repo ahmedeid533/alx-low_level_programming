cd5637b81124d780d2375da4cedca475b5090a71
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-02-27 13:23:00 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns
+ * the length of a string
+ *
+ * Return: the length of a string
+ *
+ * @s: string entry pointer
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
