202cdac853a7711f3aa87512835d1a2849d2f7b2
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-02-27 14:31:56 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,9 +13,13 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0' && str[i+1] != '\0')
+	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
+		if (str[i + 1] == '\0')
+		{
+			break;
+		}
 		i += 2;
 	}
 	_putchar('\n');
