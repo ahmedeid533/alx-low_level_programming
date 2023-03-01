8e9e674a9249e6f210c90740c998280e95d9e748
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-03-01 10:29:56 -0800

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncpy - function that copies a string
+ *
+ * @dest: destnation
+ *
+ * @src: source
+ *
+ * @n: number of chars
+ *
+ * Return: pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+		if (src[i] == '\0')
+		{
+			break;
+		}
+	}
+	dest[i] = '\0';
+	return (dest);
+}
