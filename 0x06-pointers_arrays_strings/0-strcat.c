6605740e8f4d4e7c3697de6216dde5b7720d1e0b
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-03-01 09:29:05 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat - function that concatenates two strings
+ *
+ * Return: a pointer to the resulting string dest
+ * @dest: the blace string will concat in
+ * @src: the concated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = src[j];
+	return (&dest);
+}
