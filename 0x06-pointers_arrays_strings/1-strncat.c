69c6bb8cbaffed946dee5521f76fed85c6ca3fc7
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-03-01 09:55:12 -0800

    1-strncat.c_123

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,15 +14,17 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
-	int j;
+	int j = 0;
 
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	for(j = 0; j < n; j++, i++)
+	while (j < n)
 	{
 		dest[i] = src[j];
+		i++;
+		j++;
 	}
 	dest[i] = '\0';
 	return (dest);
