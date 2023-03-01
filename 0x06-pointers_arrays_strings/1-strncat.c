8f24642f51b4d43a4ca1dcadf528f07e9f47727f
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-03-01 09:41:50 -0800

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#inlcude "main.h"
+
+/**
+ * _strncat - function that concatenates two strings
+ * it will use at most n bytes from src
+ *
+ * Return: a pointer to the resulting string dest
+ * @dest: concating string in
+ * @src: concated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j < n; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
