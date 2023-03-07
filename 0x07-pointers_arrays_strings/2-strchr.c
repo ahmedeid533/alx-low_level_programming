b75a545060e18f3f2cc235f377af63ca3ac23f48
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-03-06 17:32:59 -0800

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - that locates a character in a string
+ *
+ * @s: string
+ * @c: char
+ * Return: s
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+		i++;
+	}
+	return (NULL);
