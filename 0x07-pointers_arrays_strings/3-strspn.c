8843b42c81212f7d863c883c783b937781bc4d65
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-03-06 17:55:18 -0800

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strspn - that gets the length of a prefix
+ *
+ * @s: string
+ * @accept: prefix
+ * Return: nuber
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				i++;
+			}
+		}
+		if (accept[j] == '\0')
+		{
+			return (i+1);
+		}
+	}
+	return (0);
+}
