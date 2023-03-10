7e5c93d7ad9ca599e52bd0e7a86fd070ceac4b6b
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-03-10 08:16:44 -0800

    test

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print name
+ * @argc: number
+ * @argv: variables
+ * return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf(argv[0]);
+	printf("\n");
+	return (0);
+}
