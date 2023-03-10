7bc86f330e8eab3d3c41395d6fafbfcea1dc0cd0
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-03-10 08:38:33 -0800

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,8 +6,9 @@
  * @argv: variables
  * Return: 0
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
+	(void) argv;
 	printf("%d\n", (argc - 1));
 
 	return (0);
