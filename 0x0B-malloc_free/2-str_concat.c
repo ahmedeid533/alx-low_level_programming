1dc0f8a3bec32b6d1c00f798252ae8583c71491d
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-03-13 13:37:34 -0700

    zzZZzzZZ

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,12 +18,12 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 	{
 		s1 = malloc(1);
-		s1[0] = "";
+		s1[0] = '';
 	}
 	if (s2 == NULL)
 	{
 		s2 = malloc(1);
-		s2[0] = "";
+		s2[0] = '';
 	}
 	j = 0;
 	len = 0;
