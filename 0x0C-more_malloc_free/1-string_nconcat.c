f71da4133a0ff031b905be8049d17b9cbe72f2c9
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-03-16 16:48:55 -0700

    a7a

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,10 +12,10 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
-	int lens1 = 0;
-	int lens2 = 0;
-	int j;
-	int i;
+	unsigned int lens1 = 0;
+	unsigned int lens2 = 0;
+	unsigned int j;
+	unsigned int i;
 
 	if (s1 == NULL)
 	{
