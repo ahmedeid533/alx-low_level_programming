b0003df03d5481eec8b1ffe151fcbbd9b8d2d75f
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-03-16 16:54:09 -0700

    3ash

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,15 +18,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i;
 
 	if (s1 == NULL)
-	{
-		s1 = malloc(1);
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
-		s2 = malloc(1);
 		s2 = "";
-	}
 	while (s1[lens1] != '\0')
 		lens1++;
 	while (s2[lens2] != '\0')
@@ -34,6 +28,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n > lens2)
 		n = lens2;
 	str = malloc(lens1 + n + 1);
+	if (str == NULL)
+		return (0);
 	for (i = 0; i < lens1; i++)
 		str[i] = s1[i];
 	for (j = 0; j < n; j++, i++)
