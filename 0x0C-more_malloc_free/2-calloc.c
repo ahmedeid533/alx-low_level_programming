d05029f146f09915577c64e52b17960b5f560672
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-03-16 17:05:42 -0700

    test

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - afg
+ * @nmemb: number
+ * @size: size
+ * Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *arr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	arr = malloc(nmemb * size);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		arr[i] = 0;
+	return (arr);
+}
