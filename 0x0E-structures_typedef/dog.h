0add75ba3257a75b99bdba6677e88c2b14480372
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-03-20 11:42:33 -0700

    3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - struct
@@ -21,4 +21,4 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif 
+#endif /*DOG_H*/
