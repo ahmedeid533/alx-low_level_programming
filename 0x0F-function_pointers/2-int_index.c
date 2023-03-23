e09ea375725b8fc41a7318872307c88705845bf8
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-03-22 18:28:15 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - earches for an integer
+ * @array: arr
+ * @size: size
+ * @cmp: fun
+ * Return: integer
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	int c;
+
+	if (array == NULL || cmp == NULL)
+		return;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		c = cmp(array[size]);
+		if (c != 0)
+			return (i);
+	}
+	return (-1);
+}
