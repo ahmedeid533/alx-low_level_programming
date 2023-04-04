b7a423538a649a4f70dfc06e34a8cf975bbb7a2c
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-04-04 02:57:50 -0700

    test35

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -17,7 +17,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		return (NULL);
 	}
 	if (head == NULL)
-		*head = new;
+		head = new;
 	else
 		traverse = *head;
 	while (traverse->next != NULL)
