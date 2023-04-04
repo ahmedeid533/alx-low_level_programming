a06b3b21137d8d90aff74eb48cddaa6305027ce8
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-04-04 15:18:58 -0700

    91

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 	traverse = *head;
-	for (i = 0; i < idx; i++)
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+	}
+	for (i = 0; i < (idx - 1); i++)
 	{
 		if (traverse == NULL)
 			return (NULL);
