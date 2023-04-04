e85704832dac251e8079598bf8bd648512b20d80
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-04-04 08:11:37 -0700

    list44

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -3,14 +3,13 @@
 /**
  * free_listint - free the list
  * @head: pointer
- *
  */
 void free_listint(listint_t *head)
 {
 	listint_t *traverse;
 
 	while (head != NULL)
-	{	
+	{
 		traverse = head->next;
 		free(head);
 		head = traverse;
