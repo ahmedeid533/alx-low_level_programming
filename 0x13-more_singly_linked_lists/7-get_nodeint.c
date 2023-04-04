2a8670e633319a05219d7db5b4ada5eed57a01a2
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-04-04 14:49:40 -0700

    71

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,9 +14,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	traverse = head;
 	for (i = 0; i < index; i++)
 	{
+		traverse = traverse->next;
 		if (traverse == NULL)
 			return (NULL);
-		traverse = traverse->next;
 	}
 	return (traverse);
 }
