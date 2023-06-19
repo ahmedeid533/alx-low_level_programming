20e0bd3e07d28a5aa05b303acfaed00b07e1dbb7
ahmed eid <ahmedeidgoma333@gmail.com>
2023-06-19 08:13:06 -0700

    dog woof woof _2

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,9 +11,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog;
 
-	my_dog->name = name;
-	my_dog->age = age;
-	my_dog->owner = owner;
+	(*my_dog)->name = name;
+	(*my_dog)->age = age;
+	(*my_dog)->owner = owner;
 
 	return (my_dog);
 }
