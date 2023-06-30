60581acfcb5019a1e212f497aec8a9b915f5d289
ahmed eid <ahmedeidgoma333@gmail.com>
2023-06-30 06:38:37 -0700

    print list v0.1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,11 +8,14 @@
 size_t print_list(const list_t *h)
 {
 	size_t counter = 0;
-	list_t *head = h;
+	list_t *head = &h;
 
 	while (head != "\0")
 	{
-		printf("[%lu] %s", sizeof(head->str), head->str);
+		if (head->str == "\0")
+			pritnf("[0] (nil)");
+		else
+			printf("[%lu] %s", sizeof(head->str), head->str);
 		head = head->next;
 		counter++;
 	}
