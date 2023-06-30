7a0ee507d17941a1daaba7bcb25b35406a326965
ahmed eid <ahmedeidgoma333@gmail.com>
2023-06-30 08:52:56 -0700

    v3.2

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -6,16 +6,33 @@
  * @str: the node text
  * Return: the address of the new element
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
+	list_t *last = *head;
 	list_t *new;
+
+	if (last == NULL)
+	{
+		new = malloc(sizeof(list_t));
+		if (new == NULL)
+			return (NULL);
+		new->str = strdup(str);
+		new->len = strlen(str);
+		new->next = NULL;
+		*head = new;
+		return (new);
+	}
+
+
+	while (last->next != NULL)
+		last = last->next;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
 	new->len = strlen(str);
-	new->next = *head;
-	*head = new;
+	new->next = NULL;
+	last->next = new;
 	return (new);
 }
