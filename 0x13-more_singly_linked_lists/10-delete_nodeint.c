97a796a2ad47ccfeea9076fd8c4a3459c5430607
ahmed eid <ahmedeidgoma333@gmail.com>
2023-07-04 08:47:55 -0700

    10 v0.3

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	traverse = *head;
 	if (*head == NULL)
-		return (-1);
+		return (1);
 	if (index == 0)
 	{
 		*head = traverse->next;
