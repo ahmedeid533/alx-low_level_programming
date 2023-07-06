5fc68abfde6af7142ccad950d7f25410acf69721
ahmed eid <ahmedeidgoma333@gmail.com>
2023-07-06 08:04:18 -0700

    v5.1

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,7 +11,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned int num_bits = 0;
 	unsigned long int tmp = n;
 
-	tmp ^= m; 
+	tmp ^= m;
 	while (tmp > 0)
 	{
 		num_bits += (tmp & 1);
