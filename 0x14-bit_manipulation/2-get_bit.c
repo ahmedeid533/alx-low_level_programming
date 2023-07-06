5437d9543495795faab32a9ec7620a297f76fc46
ahmed eid <ahmedeidgoma333@gmail.com>
2023-07-06 06:50:24 -0700

    v2.1

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,7 +8,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int numBits = 0;
+	unsigned int numBits = 0;
 	unsigned long int tmp = n;
 
 	while (tmp > 0)
@@ -16,9 +16,9 @@ int get_bit(unsigned long int n, unsigned int index)
 		numBits++;
 		tmp >>= 1;
 	}
-	if (index < 0 || index >= numBits)
+	if (index >= numBits)
 	{
 		return (-1);
 	}
-	return (n & (1UL << index));
+	return ((n & (1UL << index)) ? 1 : 0);
 }
