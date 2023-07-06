f81162c35be48f761ee6b5d14dbcd3f10efae090
ahmed eid <ahmedeidgoma333@gmail.com>
2023-07-06 06:21:20 -0700

    v1.2

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -22,7 +22,7 @@ void print_binary(unsigned long int n)
 	}
 	for (int i = num_Bits - 1; i >= 0; i--)
 	{
-		unsigned long int mask = 1 << i;
+		unsigned long int mask = 1UL << i;
 
 		_putchar ((n & mask) ? '1' : '0');
 	}
