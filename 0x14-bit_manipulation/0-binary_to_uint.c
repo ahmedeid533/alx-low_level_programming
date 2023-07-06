398907efccca45251e8aa73861eeb927acb75020
ahmed eid <ahmedeidgoma333@gmail.com>
2023-07-06 05:53:06 -0700

    v0.3

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,6 +11,8 @@ unsigned int binary_to_uint(const char *b)
 	int base = 1;
 	unsigned int unit = 0;
 
+	if (b == NULL)
+		return (0);
 	for (; len >= 0; len--)
 	{
 		if (b[len] == '0' || b[len] == '1')
