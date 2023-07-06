2692c4389cb6c98ded1350ff5d65ccd6b371b3f4
ahmed eid <ahmedeidgoma333@gmail.com>
2023-07-06 07:34:45 -0700

    v2.4

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,7 +8,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (!index && index != 0U)
+	if (index >= 32)
 	{
 		return (-1);
 	}
--- 0x14-bit_manipulation/2-main.c
+++ 0x14-bit_manipulation/2-main.c
@@ -9,9 +9,11 @@
 int main(void)
 {
     int n;
-    n = get_bit(1024,11);
+    n = get_bit(1024,-3);
     printf("%d\n", n);
     n = get_bit(98, 1);
+    printf("%d\n", n);
+    n = get_bit(98, -3);
     printf("%d\n", n);
     n = get_bit(1024, 0);
     printf("%d\n", n);
