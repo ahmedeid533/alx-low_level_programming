729ab30a9dbf562e8e0ba8742e7c7425a0ca3870
ahmed eid <ahmedeidgoma333@gmail.com>
2023-07-10 06:14:21 -0700

    v1.0

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int create_file(const char *filename, char *text_content) {
+    if (filename == NULL) {
+        return -1;
+    }
+    FILE *fp = fopen(filename, "w");
+    if (fp == NULL) {
+        return -1;
+    }
+    if (text_content != NULL) {
+        size_t len = strlen(text_content);
+        if (fwrite(text_content, sizeof(char), len, fp) != len) {
+            fclose(fp);
+            return -1;
+        }
+    }
+    fclose(fp);
+    return 1;
+}
