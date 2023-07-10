ba9cb9008c1b691490f4fc044a45c91db314a3e9
ahmed eid <ahmedeidgoma333@gmail.com>
2023-07-10 06:21:12 -0700

    v1.1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,11 +3,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int create_file(const char *filename, char *text_content) {
-    if (filename == NULL) {
-        return -1;
-    }
-    FILE *fp = fopen(filename, "w");
+int create_file(const char *filename, char *text_content)
+{
+       	FILE *fp;
+	if (filename == NULL)
+	{
+		return -1;
+	}
+	fp = fopen(filename, "w");
     if (fp == NULL) {
         return -1;
     }
