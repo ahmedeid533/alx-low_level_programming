f238691ab5be7573bb4ff9cfa2d9e3f56f809c26
ahmed eid <ahmedeidgoma333@gmail.com>
2023-07-10 06:53:15 -0700

    v2.2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (access(filename, W_OK) == -1){
+	if (access(filename, W_OK) == -1)
+	{
 		return (-1);
 	}
 	if (text_content == NULL)
@@ -34,8 +35,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fwrite(text_content, sizeof(char), len, fp) != len)
 	{
 		fclose(fp);
-		return -1;
+		return (-1);
 	}
 	fclose(fp);
-	return 1;
+	return (1);
 }
