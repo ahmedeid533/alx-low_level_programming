230c8d64ba23b420562a855fc94544700cddf345
ahmed eid <ahmedeidgoma333@gmail.com>
2023-07-10 06:30:32 -0700

    v1.2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,24 +3,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * create_file - create file whit text
+ * @filename: file
+ * @text_content: as it is
+ * Return: sucess or not
+ */
 int create_file(const char *filename, char *text_content)
 {
-       	FILE *fp;
+	FILE *fp;
+
 	if (filename == NULL)
 	{
-		return -1;
+		return (-1);
 	}
 	fp = fopen(filename, "w");
-    if (fp == NULL) {
-        return -1;
-    }
-    if (text_content != NULL) {
-        size_t len = strlen(text_content);
-        if (fwrite(text_content, sizeof(char), len, fp) != len) {
-            fclose(fp);
-            return -1;
-        }
-    }
-    fclose(fp);
-    return 1;
+	if (fp == NULL)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		size_t len = strlen(text_content);
+
+		if (fwrite(text_content, sizeof(char), len, fp) != len)
+		{
+			fclose(fp);
+			return (-1);
+		}
+	}
+	fclose(fp);
+	return (1);
 }
