e2766818611db7ee048315b95ef6bfd4a6037693
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-08-17 20:05:28 +0300

    CV0.60

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - summ nodes
+ * 
+ * @head: header
+ * Return: summition
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum;
+
+	sum = 0;
+	if (head == NULL)
+		return (sum);
+
+	while (head->prev != NULL)
+			head = head->prev;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	sum += head->n;
+	return (sum);
+}
