09e84e6cbfde050bcde665e2efbd572f782ccdee
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-08-17 18:49:14 +0300

    CV0.13

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - length
+ * 
+ * @h: the head
+ * Return: the size
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	dlistint_t *current;
+	size_t length;
+
+	current = (dlistint_t *)h;
+	length = 0;
+	while (current != NULL)
+	{
+		current = current->next;
+		length++;
+	}
+	return (length);
+}
