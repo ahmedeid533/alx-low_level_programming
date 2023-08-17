a2fb25c84ee626aa6f316ccd0da2081ca4d8dece
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-08-17 18:22:24 +0300

    CV0.1

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * print_dlistint - print dllist
+ * @h: head 
+ * Return: size 
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	dlistint_t *current;
+	size_t length;
+
+	current = h;
+	length = 0;
+	while (current != NULL)
+	{
+		printf("%i",current->n);
+		current = current->next;
+		length++;
+	}
+}
