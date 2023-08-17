32df80b27c9ef6f24a82c78e5fe1462342fceccf
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-08-17 19:46:02 +0300

    CV0.40

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+void free_dlistint(dlistint_t *head)
+{
+	if (head == NULL)
+		return;
+	while (head->next)
+	{
+		head = head->next;
+		free(head->prev)	
+	}
+	free(head);
+}
