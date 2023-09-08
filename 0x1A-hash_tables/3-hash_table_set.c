a3266dbf220d00fcca7f1951a4879025fd2c1fcb
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-09-08 16:16:31 +0300

    C0x1A.013

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -30,9 +30,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 
 	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL) {
+	if (new_node == NULL)
 		return 0;
-	}
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 	new_node->next = ht->array[idx];
