05c88e6cf457c74c66055f777be34f3eaf3875ba
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-09-08 15:07:39 +0300

    C0x1A.001

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,12 +12,13 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
-	hash_node_t *table[size];
+	hash_node_t **table;
 
 	new_table = malloc(sizeof(hash_table_t));
+	table = malloc(sizeof(hash_node_t) * size);
 
 	new_table->size = size;
-	new_table->array = &table[0];
+	new_table->array = table;
 
 	return (new_table);
 }
