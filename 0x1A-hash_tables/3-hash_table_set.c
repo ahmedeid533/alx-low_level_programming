d843d47f8e558fd9ac3279c5f3c3d7b6937dc0f7
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-09-08 16:15:25 +0300

    C0x1A.012

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,6 +13,21 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node;
+	unsigned long int idx, i;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	idx = key_index((const unsigned char *)key, ht->size);
+	for (i = idx; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = strdup(value);
+			return (1);
+		}
+	}
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL) {
@@ -20,7 +35,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	new_node->next = ht->array[key_index((const unsigned char *)key, ht->size)];
-	ht->array[key_index((const unsigned char *)key, ht->size)] = new_node;
+	new_node->next = ht->array[idx];
+	ht->array[idx] = new_node;
 	return 1;
 }
