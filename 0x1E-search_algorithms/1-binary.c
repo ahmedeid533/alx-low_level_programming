7c77533f3092f6c7b7162f3d943baabf341099cd
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-12-12 09:56:31 +0200

    search_algos v0.01

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,33 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in a sorted array of integers
+ * using the Binary search algorithm
+ *
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: index where value is located, or -1 if not found or array is NULL
+ */
+int binary_search(int *array, size_t size, int value) {
+	int low = 0;
+	int high = size - 1;
+
+	if (array == NULL) {
+		return -1;
+	}
+	while (low <= high) {
+		int mid = (low + high) / 2;
+
+		if (array[mid] == value) {
+			return mid;
+		} else if (array[mid] < value) {
+			low = mid + 1;
+		} else {
+			high = mid - 1;
+		}
+	}
+	return -1;
+}
+	
