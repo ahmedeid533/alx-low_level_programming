f2baece9b9db5a689fc6267f6293510e0c623a58
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-12-12 10:14:22 +0200

    search_algos v0.04

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,17 +11,24 @@
  *
  * Return: index where value is located, or -1 if not found or array is NULL
  */
-int linear_search(int *array, size_t size, int value) {
+int linear_search(int *array, size_t size, int value)
+{
 	size_t i;
 
-	if (array == NULL) {
+	if (array == NULL)
+	{
 		return -1;
 	}
-	for (i = 0; i < size; i++) {
+
+	for (i = 0; i < size; i++)
+	{
 		printf("Value checked array[%llu] = [%d]\n", i, array[i]);
-		if (array[i] == value) {
+
+		if (array[i] == value)
+		{
 			return i;
 		}
 	}
+
 	return -1;
 }
