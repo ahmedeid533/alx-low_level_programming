bf1822f689fe93b6e89b138ba44e7e0f82256ecd
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-12-12 10:32:16 +0200

    search_algos v0.08

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -21,12 +21,11 @@ int linear_search(int *array, size_t size, int value)
 	}
 	for (; i < size; i++)
 	{
-		printf("Value checked array[%llu] = [%d]\n", i, array[i]);
+		printf("Value checked array[%lu] = [%d]\n", (unsigned long) i, array[i]);
 		if (array[i] == value)
 		{
 			return (i);
 		}
 	}
-
 	return (-1);
 }
